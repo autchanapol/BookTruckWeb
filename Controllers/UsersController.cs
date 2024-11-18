077214e0db68227913ba3492cc4a925e1a3ac154
autchanapol <autchanapol.n@gmail.com>
2024-11-18 16:06:46 +0700

    LoginPage

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -51,6 +51,12 @@ namespace BookTruckWeb.Controllers
             return View();
         }
 
+        // GET: Users/Create
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
         // POST: Users/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
