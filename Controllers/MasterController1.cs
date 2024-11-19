9c3830a6d739a30fe03c0c8b489d15663f4e9a32
Pantep150 <pantep150@gmail.com>
2024-11-19 22:47:04 +0700

    Please Delete Update

--- Controllers/MasterController1.cs
+++ Controllers/MasterController1.cs
@@ -12,5 +12,9 @@ namespace BookTruckWeb.Controllers
         {
             return View();
         }
+        public IActionResult CustomersEdit()
+        {
+            return View();
+        }
     }  
 }
